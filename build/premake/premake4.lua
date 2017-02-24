d8f1ec1eedf80d1f0c099a94d2375009dc762795
wish_wish <12092020@qq.com>
2017-02-24 17:15:28 +0800

    add d

--- build/premake/premake4.lua
+++ build/premake/premake4.lua
@@ -203,7 +203,7 @@ function project_set_build_flags()
 		-- use native wchar_t type (not typedef to unsigned short)
 		flags { "NativeWChar" }
 		--flags { "Unicode" }
-		--defines { "_UNICODE","UNICODE" }
+		--defines { "_UNICODE","UNICODE","_UNICODE_" }
 	else	-- *nix
 
 		-- TODO, FIXME: This check is incorrect because it means that some additional flags will be added inside the "else" branch if the
